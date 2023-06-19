8f14ac43176b1a2af2b8cad1bd4c8d1fc581e2b6
samx81 <sam89881@gmail.com>
2023-06-19 09:51:13 +0800

    mosec with msgpack

--- client.py
+++ client.py
@@ -3,16 +3,24 @@ from http import HTTPStatus
 import requests
 from datasets import load_dataset
 
+import msgpack  # type: ignore
+
 
 def main():
     # ds = load_dataset(
     #     "hf-internal-testing/librispeech_asr_dummy", "clean", split="validation"
     # )
     # sample = ds[0]["audio"]['path']
-    sample = "./example/1272-128104-0002.wav"
+    # sample = "./example/1272-128104-0002.wav"
+    sample = "/mnt/c/Users/Sam/Documents/ezai/debug_wavs/7554_chapters_sentence_412282_1686639328873.wav"
     print(sample)
     with open(sample, "rb") as f:
-        resp = requests.post("http://localhost:8000/inference", data=f)
+        req = {
+            "binary": f.read(),
+            "id": "1",
+        }
+
+        resp = requests.post("http://localhost:8080/inference", data=msgpack.packb(req))
     if resp.status_code == HTTPStatus.OK:
         print(resp.content.decode("utf-8"))
     else:
--- main.py
+++ main.py
@@ -2,40 +2,62 @@ import torch, soundfile, io
 import numpy as np
 from transformers import WhisperProcessor
 from mosec import Server, Worker, get_logger
+import mosec.errors
 from typing import List, Any
+import msgspec
+from mosec.mixin import TypedMsgPackMixin
 
 import whisperx
 logger = get_logger()
-class Preprocess(Worker):
+
+class Request(msgspec.Struct):
+    id: str
+    binary: bytes
+
+class Validation(TypedMsgPackMixin, Worker):
+    def forward(self, data: Request):
+        if data.id != "1":
+            raise mosec.errors.ValidationError("id not 1")
+        
+        return data.binary
+
+class Preprocess(TypedMsgPackMixin, Worker):
     def __init__(self):
         self.processor = WhisperProcessor.from_pretrained(
             "openai/whisper-medium", language="Chinese", task="transcribe"
             )
 
-    def deserialize(self, data: bytes) -> any:
+    def forward(self, data):
         with io.BytesIO(data) as byte_io:
             array, sampling_rate = soundfile.read(byte_io)
         if len(array) == 2 and array.shape[1] == 2:
             # conbime the channel
             array = np.mean(array, 1)
-        return {"array": array, "sampling_rate": sampling_rate}
+        logger.info({"array": array, "sampling_rate": sampling_rate})
 
-    def forward(self, data):
+        audio = {"array": array, "sampling_rate": sampling_rate}
+
         res = self.processor(
-            data["array"], sampling_rate=data["sampling_rate"], return_tensors="pt"
+            audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt"
         )
         logger.info('Preprocess finished')
         return res.input_features
 
 
-class Inference(Worker):
+class Inference(TypedMsgPackMixin, Worker):
     def __init__(self):
-        device = "cpu" 
+        # device = "cpu" 
+        # compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy)
+        model_type = "small.en"
+        # model_type = "medium"
+        device = "cuda" 
         compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy)
-        self.model = whisperx.load_model("medium", device, compute_type=compute_type, language="zh")
+        
+        self.model = whisperx.load_model(model_type, device, compute_type=compute_type, language="en")
+        logger.info(f'Model loaded {model_type} {device} {compute_type}')
 
         # Warm up
-        _ = self.model.transcribe("example/1272-128104-0000.wav")
+        _ = self.model.transcribe("1272-128104-0000.wav")
         logger.info('Warm up finished')
 
     def forward(self, data: List[Any]) -> List[Any]:
@@ -45,28 +67,16 @@ class Inference(Worker):
 
         ids = self.model.model.generate_segment_batched(data, self.model.tokenizer, self.model.options)
         logger.info("Inference Finished")
+        
         return ids
     
-    def serialize(self, data: str) -> bytes:
-        return data.encode("utf-8")
-
-class Postprocess(Worker):
-    def __init__(self):
-        self.processor = WhisperProcessor.from_pretrained(
-            "openai/whisper-medium", language="Chinese", task="transcribe"
-            )
-
-    def forward(self, data):
-        logger.info('Postprocess begin')
-        return data
-    
-
     def serialize(self, data: str) -> bytes:
         return data.encode("utf-8")
 
 
 if __name__ == "__main__":
     server = Server()
+    server.append_worker(Validation, num=2)
     server.append_worker(Preprocess, num=2)
-    server.append_worker(Inference, num=2, max_batch_size=16, max_wait_time=1000)
+    server.append_worker(Inference, num=1, max_batch_size=8)
     server.run()
