95bfbc8bc27a3caf15d8d5462d7f757ce41a6af2
teinhonglo <teinhonglo@gmail.com>
2024-11-19 11:16:02 +0800

    [Codes] added the gop module to the app.py

--- app.py
+++ app.py
@@ -247,26 +247,31 @@ class Inference(TypedMsgPackMixin, Worker):
             prompt_ids = self.processor(prompt_phones).input_ids
             prompt_ids = torch.tensor(prompt_ids, device=self.device).unsqueeze(0)
         
-        gop_dict = self.compute_gop(labels=prompt_ids, logits=logits, loss=loss)
-
+        gop_sd_result = self.compute_gop(labels=prompt_ids, logits=logits, loss=loss)
+        
+        # ditate
         eval_result = per_result.split("\n")[:-2][-1].split()[1:]
-        eval_result_noins = [ er for er in eval_result if er != "I"]
+        eval_sd_result = [ er for er in eval_result if er != "I"]
 
         dictate_list = []
+        gop_list = []
         prompt_list = prompt_phones.split()
 
         for word_info in word_boundaries_list:
             word, idx_info = word_info
             start_idx, end_idx = idx_info
             phones = prompt_phones[start_idx:end_idx]
-            phones_eval_list = [[prompt_list[i], eval_result_noins[i]] for i in range(start_idx,end_idx)]
+            phones_eval_list = [[prompt_list[i], eval_sd_result[i]] for i in range(start_idx,end_idx)]
+            phones_gop_list = [[prompt_list[i], gop_sd_result[i]] for i in range(start_idx,end_idx)]
 
             dictate_list.append([word, phones_eval_list])
+            gop_list.append([word, phones_gop_list])
         
         capt_dict = {
             "Dictate": dictate_list, 
             "Transcript": predict_phones, 
-            "Feedback": fb_result
+            "Feedback": fb_result,
+            "GOP": gop_list
         }
         
         logger.debug(capt_dict)
@@ -278,22 +283,66 @@ class Inference(TypedMsgPackMixin, Worker):
         return data.encode("utf-8")
      
     def compute_gop(self, labels, logits, loss):
-        log_like_total = loss
-        labels = labels.clone().squeeze(0)
-        logits = logits.clone().squeeze(0)
+        # https://github.com/frank613/CTC-based-GOP/blob/main/generate-GOP/gop-ctc-af-SD.py
+        gop_sd_result = []
+        ll_self = loss.detach().cpu()
+        labels = labels.clone().detach().cpu().squeeze(0)
+        logits = logits.clone().detach().cpu().squeeze(0)
         
         pids = labels.tolist()
         num_labels = len(labels)
         post_mat = logits.softmax(dim=-1).transpose(0,1)
         
+        ll_self = self.ctc_loss(post_mat, labels)
+        
         for i, pid in enumerate(pids):
-            gop_feats = [log_like_total]
-            ll_denom = ctc_loss_denom(post_mat, labels, i, blank=0)
+            ll_denom = self.ctc_loss_denom(post_mat, labels, i, blank=0)
             gop = -ll_self + ll_denom
             print(pid, gop)
+            gop_sd_result.append(gop.item())
             
-        return gop
-            
+        return gop_sd_result
+    
+    def ctc_loss(self, params, seq, blank=0):
+        """
+        CTC loss function.
+        params - n x m matrix of n-D probability distributions(softmax output) over m frames.
+        seq - sequence of phone id's for given example.
+        Returns objective, alphas and betas.
+        """
+        seqLen = seq.shape[0] # Length of label sequence (# phones)
+        numphones = params.shape[0] # Number of labels
+        L = 2*seqLen + 1 # Length of label sequence with blanks
+        T = params.shape[1] # Length of utterance (time)
+        
+        alphas = torch.zeros((L,T)).double()
+        
+        # Initialize alphas and forward pass 
+        alphas[0,0] = params[blank,0]
+        alphas[1,0] = params[seq[0],0]
+        
+        for t in range(1,T):
+            start = max(0,L-2*(T-t)) 
+            end = min(2*t+2,L)
+            for s in range(start,L):
+                l = int((s-1)/2)
+                # blank
+                if s%2 == 0:
+                    if s==0:
+                        alphas[s,t] = alphas[s,t-1] * params[blank,t]
+                    else:
+                        alphas[s,t] = (alphas[s,t-1] + alphas[s-1,t-1]) * params[blank,t]
+                # same label twice
+                elif s == 1 or seq[l] == seq[l-1]:
+                    alphas[s,t] = (alphas[s,t-1] + alphas[s-1,t-1]) * params[seq[l],t]
+                else:
+                    alphas[s,t] = (alphas[s,t-1] + alphas[s-1,t-1] + alphas[s-2,t-1]) \
+                        * params[seq[l],t]
+        
+        llForward = torch.log(alphas[L-1, T-1] + alphas[L-2, T-1])
+    
+        return -llForward
+        
     def ctc_loss_denom(self, params, seq, pos, blank=0):
         """
         CTC loss function.
@@ -340,7 +389,7 @@ class Inference(TypedMsgPackMixin, Worker):
                     if s==0:
                         alphas[s,t,0] = alphas[s,t-1,0] * params[blank,t]
                     else:
-                        sum = check_arbitrary(alphas, s-1, t-1, [blank]) 
+                        sum = self.check_arbitrary(alphas, s-1, t-1, [blank]) 
                         if sum: ## the first blank(for current t) after the arbitrary state,need to collect the probability from the additional dimension
                             ##in this version no need to remove for t=1, because state 2 is not initialized anymore
                             alphas[s,t,0] = (alphas[s,t-1,0] + sum) * params[blank,t]
@@ -353,7 +402,7 @@ class Inference(TypedMsgPackMixin, Worker):
                         alphas[s,t,0] = (alphas[s,t-1,0] + alphas[s-1,t-1,0] + alphas[s-2,t-1,0]) \
                             * params[seq[l],t]
                 elif pos == l-1: #last token is the arbitrary token, need to collect the probability from the additional dimension, and also consider the skip paths
-                    sum = check_arbitrary(alphas, s-2, t-1, [blank,seq[l]])  ##remove the entry of the blank and the  "l"th token in the last dim, because it's already covered in other terms with the same path
+                    sum = self.check_arbitrary(alphas, s-2, t-1, [blank,seq[l]])  ##remove the entry of the blank and the  "l"th token in the last dim, because it's already covered in other terms with the same path
                     if l-2 < 0 or seq[l-2] == seq[l]: ###dont allow token skip
                         skip_token = 0
                     else:
@@ -377,13 +426,30 @@ class Inference(TypedMsgPackMixin, Worker):
     
                         alphas[s,t,:] = (alphas[s,t-1,:].view(1,-1) * params[:,t].view(-1,1) * mask_ins).sum(-1) + skip_prob + empty_prob
              
-        sum = check_arbitrary(alphas, L-2, T-1, [blank])    
+        sum = self.check_arbitrary(alphas, L-2, T-1, [blank])    
         if sum: # last label is arbitrary,  we need the skip path alphas[L-3, T-1, 0] + alphas[L-4, T-1, 0]
             llForward = torch.log(alphas[L-1, T-1, 0] + sum + alphas[L-3, T-1, 0] + alphas[L-4, T-1, 0])
         else:
             llForward = torch.log(alphas[L-1, T-1, 0] + alphas[L-2, T-1, 0])
     
         return -llForward
+    
+    def check_arbitrary(self, in_alphas, s, t, zero_pos=[]):
+        '''
+        ##check if the last dim > 0, return the sum of last dimension (collect the posterior for each possible tokens),the zero_pos is excluded in the sum.
+        ##zero pos here starst from 0
+        '''
+        if in_alphas[s,t].sum() > 0:
+            if len(zero_pos) != 0:
+                mask = torch.ones_like(in_alphas[s,t])
+                for i in zero_pos:
+                    mask[i] = 0
+                return sum(in_alphas[s,t][mask.bool()])
+            else:
+                return sum(in_alphas[s,t][:])
+        else:
+            return False
+
 
 if __name__ == "__main__":
     server = Server()
